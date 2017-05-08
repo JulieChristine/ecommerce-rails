3bb1aab5e61f3f501c744a256de9c8970d0cfd3a
JulieChristine <julie.wolsak@gmail.com>
2017-05-08 19:53:50 +0800

    add seeds file, controller product and product route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Product < ActiveRecord::Base
+list = [
+    [ "10.5", "orange" ],
+    [ "7", "apple" ],
+    [ "8", "banana" ],
+    [ "20", "pineapple" ],
+    [ "25", "pen" ],
+    [ "15.5", "mango" ],
+    [ "40", "durian" ]
+  ]
+
+  list.each do |price, name|
+    Product.create( price: price , name: name)
+  end
+end
