ebc4a4cdf7cfb0a41d8b9d61034c2e403bb81353
JulieChristine <julie.wolsak@gmail.com>
2017-05-09 11:41:41 +0800

    add the loging for sign up, add adding an item to the chart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ApplicationRecord
-  has_many :order_items, dependent: destroy
+  has_many :order_items, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
-  resources :order_items 
+  resources :order_items
 
-  root to: "home#index"
+  root to: "products#index"
 end
